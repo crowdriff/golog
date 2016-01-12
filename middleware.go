9149b3476eb2a220932a54718697f9c08ca8c52f
ryanfowler <ryan.fowler19@gmail.com>
2016-01-12 12:23:04 -0500

    Add middleware tests

--- middleware.go
+++ middleware.go
@@ -6,9 +6,9 @@ import (
 	"time"
 )
 
-// ResponseWriter represents an object that wraps an http.ResponseWriter to
+// responseWriter represents an object that wraps an http.ResponseWriter to
 // record the response code returned, as well as the size of the response body.
-type ResponseWriter struct {
+type responseWriter struct {
 	http.ResponseWriter
 	Code int
 	Size int
@@ -16,14 +16,14 @@ type ResponseWriter struct {
 
 // WriteHeader records the code and calls the underlying ResponseWriter's
 // WriteHeader method.
-func (rw *ResponseWriter) WriteHeader(c int) {
+func (rw *responseWriter) WriteHeader(c int) {
 	rw.Code = c
 	rw.ResponseWriter.WriteHeader(c)
 }
 
 // Write records the size of the bytes written and calls the underlying
 // ResponseWriter's Write method.
-func (rw *ResponseWriter) Write(b []byte) (int, error) {
+func (rw *responseWriter) Write(b []byte) (int, error) {
 	n, err := rw.ResponseWriter.Write(b)
 	rw.Size += n
 	return n, err
@@ -31,7 +31,7 @@ func (rw *ResponseWriter) Write(b []byte) (int, error) {
 
 // Flush calls the underlying ResponseWriter's Flush method, if it conforms to
 // the http.Flusher.
-func (rw *ResponseWriter) Flush() {
+func (rw *responseWriter) Flush() {
 	if f, ok := rw.ResponseWriter.(http.Flusher); ok {
 		f.Flush()
 	}
@@ -40,15 +40,17 @@ func (rw *ResponseWriter) Flush() {
 // logRequest logs a request to standard out with the provided resonse writer,
 // request, and starting time.
 // An example to show the format is:
-// 2016/01/12 10:21:38 [golog] GET /path?query=10 (1024) 4580341
-// <time> [<server name>] <method> <path> (<bytes written>) <microseconds>
+// 2016/01/12 10:21:38 [golog] 200 GET /path?query=10 (1024) 4580
+// <time> [<server name>] <status> <method> <path> (<bytes written>) <microseconds>
 //
 // Note: the provided time should be UTC.
-func (l *Logger) logRequest(w *ResponseWriter, r *http.Request, start time.Time) error {
+func (l *Logger) logRequest(w *responseWriter, r *http.Request, start time.Time) error {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 
 	l.formatHeader(start)
+	l.buf = append(l.buf, strconv.Itoa(w.Code)...)
+	l.buf = append(l.buf, ' ')
 	l.buf = append(l.buf, r.Method...)
 	l.buf = append(l.buf, ' ')
 	l.buf = append(l.buf, r.URL.RequestURI()...)
@@ -68,8 +70,9 @@ func LogRequestMiddleware(l *Logger) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		f := func(w http.ResponseWriter, r *http.Request) {
 			t := time.Now().UTC()
-			h.ServeHTTP(&ResponseWriter{w, 0, 0}, r)
-			l.log(r.Method+" "+r.URL.RequestURI()+" ", t)
+			wr := &responseWriter{w, 0, 0}
+			h.ServeHTTP(wr, r)
+			l.logRequest(wr, r, t)
 		}
 		return http.HandlerFunc(f)
 	}
---
+++ middleware_test.go
@@ -0,0 +1,50 @@
+package golog_test
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+
+	. "github.com/crowdriff/golog"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type fakeHTTPHandler struct {
+	Code int
+	Size int
+}
+
+func (h *fakeHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(h.Code)
+	w.Write(make([]byte, h.Size))
+}
+
+var _ = Describe("Middleware", func() {
+	It("should log the request with middleware", func() {
+		var buf bytes.Buffer
+		l := NewLogger("golog")
+		l.Out = &buf
+
+		h := &fakeHTTPHandler{http.StatusOK, 100}
+		f := LogRequestMiddleware(l)(h)
+
+		wr := httptest.NewRecorder()
+		r, err := http.NewRequest("GET", "http://localhost/path?query=10", nil)
+		Ω(err).ShouldNot(HaveOccurred())
+		f.ServeHTTP(wr, r)
+
+		out := buf.String()
+		out = parseDate(out)
+		out = parseServerName(out, "golog")
+		reqStr := "200 GET /path?query=10 (100) "
+		ok := strings.HasPrefix(out, reqStr)
+		Ω(ok).Should(BeTrue())
+		i, err := strconv.Atoi(out[len(reqStr) : len(out)-1])
+		Ω(err).ShouldNot(HaveOccurred())
+		Ω(i).Should(BeNumerically(">=", 0))
+	})
+})
